fca8c3486518d98d3f7fa94091b7ddbfa46aac76
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2019-08-17 16:23:03 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,35 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='PACKAGENAME',
+   name='siptom',
    version='1.0',
    description='An implementation of game theory of mind in a agent based framework following the implementation of Devaine, et al. (2017).',
-   license="MIT",
+   license='Apache License 2.0',
    long_description=long_description,
-   author='Man Foo',
-   author_email='foomail@foo.com',
-   url="http://www.foopackage.com/",
-   packages=['foo'],  #same as name
-   install_requires=['bar', 'greek'], #external packages as dependencies
-   scripts=[
-            'scripts/cool',
-            'scripts/skype',
-           ]
+   long_description_content_type="text/markdown",
+   author='Kenneth C. Enevoldsen and Peter T. Waade',
+   author_email='kennethcenevoldsen@gmail.com',
+   url="https://github.com/KennethEnevoldsen/siptom",
+   packages=['siptom'],  #same as name
+   install_requires=['numpy', 'pandas'], #external packages as dependencies
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='theory-of-mind tom game-theory',
 )
