22b49344f218a9e26292caee7ee16f6bd736ddae
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-12-07 14:58:14 +0100

    added vers. number

--- tomsup/__init__.py
+++ tomsup/__init__.py
@@ -2,3 +2,5 @@ from tomsup.payoffmatrix import *
 from tomsup.utils import valid_agents, create_agents
 from tomsup.agent import *
 import tomsup.plot as plot
+
+from tomsup._version import __version__
