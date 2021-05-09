fcf5f1950a00477d4cf07c667768bbc03baafd69
PTWaade <peter@waade.net>
2021-05-09 20:23:52 +0200

    minor

--- simulations/behaviour_test.py
+++ simulations/behaviour_test.py
@@ -11,9 +11,24 @@ sys.path.append('/Users/ptwaade/Desktop/Uni/tomsup/tomsup_package')
 import tomsup as ts
 
 #P1 forced choices:
-forced_choices_p1 = [0, 1, 0, 1, 0, 1, 1, 1, 1, 0]
+forced_choices_p1 = [0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1]
 #P2 forced choices: 
-forced_choices_p2 = [1, 1, 0, 0, 1, 0, 1, 1, 0, 1]
+forced_choices_p2 = [0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0]
+
+#P1 forced choices:
+forced_choices_p1 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+#P2 forced choices: 
+forced_choices_p2 = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+
+# #P1 forced choices:
+# forced_choices_p1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# #P2 forced choices: 
+# forced_choices_p2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+# #P1 forced choices:
+# forced_choices_p1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# #P2 forced choices: 
+# forced_choices_p2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # Simulation settings
 n_trials = 10
@@ -22,28 +37,39 @@ n_trials = 10
 penny_comp = ts.PayoffMatrix(name='penny_competitive')
 
 # Create agents
-tom_1 = ts.TOM(level = 2, volatility= -2,  b_temp= -1,
-               bias = 0, dilution = None, save_history = True)
+player_1 = ts.RB()
 
-tom_2 = ts.TOM(level = 2, volatility= -2,  b_temp= -1,
+player_2 = ts.TOM(level = 1, volatility= -2,  b_temp= -1,
                bias = 0, dilution = None, save_history = True)
 
 # Reset agents before start
-tom_1.reset()
-tom_2.reset()
-
-# Set choices to None in the first roun
-prev_choice_p1 = None
-prev_choice_p2 = None
+player_1.reset()
+player_2.reset()
 
 # Go through each trial one at a time
-for trial in range(1, n_trials):
-    # Make choices
-    choice_1 = tom_1.compete(p_matrix=penny_comp, agent=0, 
-                            op_choice=prev_choice_p1)
-    choice_2 = tom_2.compete(p_matrix=penny_comp, agent=1,
-                            op_choice=prev_choice_p2)
+for trial in range(n_trials):
 
-    # Save choices for next round
-    prev_choice_1tom = forced_choices_p1[trial]
-    prev_choice_2tom = forced_choices_p2[trial]
+    print(trial)
+
+    # Save opponent choices and save
+    prev_choice_p1 = forced_choices_p1[trial]
+    prev_choice_p2 = forced_choices_p2[trial]
+    
+    # Make choices
+    choice_1 = player_1.compete(p_matrix=penny_comp, agent=0,
+                                op_choice=prev_choice_p1)
+    choice_2 = player_2.compete(p_matrix=penny_comp, agent=1,
+                                op_choice=prev_choice_p2)
+
+    # Reset own choices
+    player_1.choice = forced_choices_p1[trial+1]
+    player_2.choice = forced_choices_p2[trial+1]
+
+    print('SELF: {}'.format(prev_choice_p2))
+    print('OPPONENT: {}'.format(prev_choice_p1))
+    player_2.print_internal()
+
+
+
+
+
