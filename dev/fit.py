d39cdccc8f16f0c025becabe265dd7ba877e810c
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-08-06 16:19:50 +0200

    update

--- dev/fit.py
+++ dev/fit.py
@@ -4,12 +4,18 @@ This script fits the k-ToM model to data using scipy.optimize()
 import sys
 from functools import partial
 sys.path.append("..")
+sys.path.append(".")
+from wasabi import msg
+
+
 
 import numpy as np
 from scipy.optimize import minimize
 
 import tomsup as ts
 
+
+# generating some sample data
 group = ts.create_agents(["1-ToM", "2-ToM"])
 penny = ts.PayoffMatrix("penny_competitive")
 results = group.compete(
@@ -46,31 +52,32 @@ def forced_choice_competition(
 
 
 def func_to_minimize(
-    k: int = 1,                    # optimizable variable
-    volatility: float = -2,        # optimizable variable
-    b_temp: float = -1,            # optimizable variable
-    bias: float = 0,               # optimizable variable
-    dilution: float = 0,           # optimizable variable
+    k: int,                        # optimizable variable
+    volatility: float,             # optimizable variable
+    b_temp: float,                 # optimizable variable
+    bias: float,                   # optimizable variable
+    dilution: float,               # optimizable variable
     choices_agent=results.choice_agent0,    # known variables
     choices_opponent=results.choice_agent1, # known variables
     opponent = group.get_agent("2-ToM"),    # known variables
     agent_pov=0,                            # known variables
 ):
-
-    p_choices = forced_choice_competition(
-        agent0=ts.create_agents(
+    agent0=ts.create_agents(
             f"{k}-ToM", 
             volatility=volatility, 
             b_temp=b_temp, 
             bias=bias,
             dilution=dilution
-        ),
+        )
+
+    p_choices = list(forced_choice_competition(
+        agent0=agent0,
         agent1=opponent,
         choices_a0=choices_agent,
         choices_a1=choices_opponent,
         p_matrix=penny,
-        agent=agent_pov,
-    )
+        agent_pov=agent_pov,
+    ))
 
     # euclidian distance between actual choices and probability of the simulated agents
     return np.linalg.norm(np.array(choices_agent) - np.array(p_choices))
@@ -79,17 +86,26 @@ def func_to_minimize(
 def func_to_minimize_wrapper(params, k=1):
     """a wrapper function to map between minimize"""
     return func_to_minimize(k=k, 
-                            volatility = params[1],
-                            b_temp = params[2],
-                            bias= params[3],
-                            dilution=params[4],
+                            volatility = params[0],
+                            b_temp = params[1],
+                            bias= params[2],
+                            dilution=params[3],
                             )
 
 solutions = []
-for i in range(0, 4):
-    f = partial(func_to_minimize_wrapper, k=i)
+
+# bounds = [(-3, 0.001), (-3, 0.001), (-1, 1), (1, 0)]
+
+bounds = [(-2.2, -1.8), (-1.2, -0.8), (-0,2, 0.2), (0, 0.01)]
+
+
+for k in range(0, 3):
+    msg.info(f"Running minimization for {k}-ToM")
+    f = partial(func_to_minimize_wrapper, k=k)
     solution = minimize(f, 
-            [1, -2, -1, 0, 0], # need restrictions on these!
+            [-2, -1, 0, 0], # need restrictions on these!
             method="nelder-mead",
-            bounds = [(0.001, -3), (0.001, -3), (1, -1), (0, 1)])
-    solutions.append(solution)
+            bounds = bounds,
+            options={"max_iter": 100})
+    solutions.append({"agent": f"{k}-ToM", "solution": solution})
+solutions
