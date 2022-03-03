01e9837a5a5564c8146e357b3f466042fb63ae01
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-03-03 09:52:03 +0100

    fixed commas error in setup

--- tomsup/about.py
+++ tomsup/about.py
@@ -1,3 +1,3 @@
 __title__ = "tomsup"
-__version__ = "1.1.6"  # the ONLY source of version ID
+__version__ = "1.1.7"  # the ONLY source of version ID
 __download_url__ = "https://github.com/KennethEnevoldsen/tomsup"
